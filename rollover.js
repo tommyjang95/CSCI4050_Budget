6dc92ea7d3d64a6533151c49e940820eebd3c8c8
tommyjang95 <jj31899@uga.edu>
2017-11-28 01:08:05 -0500

    Update rollover.js

--- rollover.js
+++ rollover.js
@@ -6,37 +6,107 @@
 
 function calculate() {
 
-
+    //Initializes user input
     var input = document.getElementById("rolloverincome").value;
-
-    //alert(input);
-
-    var monthlyBudget = 0;
-
     var month1 = document.getElementById("month").value;
 
 
-    if(month1 == "january") {
+    var monthlyBudget = 0;
+
+
+    //Divides the total month budget(user input) into individual budgets according what month(user input)
+    if(month1 == "january" || month1 == "January") {
         monthlyBudget = input/31
-        alert(monthlyBudget);
-    } else {
+        var calculatedBudget = Math.floor(monthlyBudget* 100) / 100
+        alert(calculatedBudget);
+    } else if (month1 == ("february") || month1 == "February") {
+        monthlyBudget = input/28
+        var calculatedBudget = Math.floor(monthlyBudget* 100) / 100
+        alert(calculatedBudget);
+    } else if (month1 == ("march") || month1 == "March") {
+        monthlyBudget = input/31
+        var calculatedBudget = Math.floor(monthlyBudget* 100) / 100
+        alert(calculatedBudget);
+    } else if (month1 == ("april") || month1 == "April") {
         monthlyBudget = input/30
-        alert(monthlyBudget);
+        var calculatedBudget = Math.floor(monthlyBudget* 100) / 100
+        alert(calculatedBudget);
+    } else if (month1 == ("may") || month1 == "May") {
+        monthlyBudget = input/31
+        var calculatedBudget = Math.floor(monthlyBudget* 100) / 100
+        alert(calculatedBudget);
+    } else if (month1 == ("june") || month1 == "June") {
+        monthlyBudget = input/30
+        var calculatedBudget = Math.floor(monthlyBudget* 100) / 100
+        alert(calculatedBudget);
+    } else if (month1 == ("july") || month1 == "July") {
+        monthlyBudget = input/31
+        var calculatedBudget = Math.floor(monthlyBudget* 100) / 100
+        alert(calculatedBudget);
+    } else if (month1 == ("august") || month1 == "August") {
+        monthlyBudget = input/31
+        var calculatedBudget = Math.floor(monthlyBudget* 100) / 100
+        alert(calculatedBudget);
+    } else if (month1 == ("september") || month1 == "September") {
+        monthlyBudget = input/30
+        var calculatedBudget = Math.floor(monthlyBudget* 100) / 100
+        alert(calculatedBudget);
+    } else if (month1 == ("october") || month1 == "October") {
+        monthlyBudget = input/31
+        var calculatedBudget = Math.floor(monthlyBudget* 100) / 100
+        alert(calculatedBudget);
+    } else if (month1 == ("november") || month1 == "November") {
+        monthlyBudget = input/30
+        var calculatedBudget = Math.floor(monthlyBudget* 100) / 100
+        alert(calculatedBudget);
+    } else if (month1 == ("december") || month1 == "December") {
+        monthlyBudget = input/31
+        var calculatedBudget = Math.floor(monthlyBudget* 100) / 100
+        alert(calculatedBudget);
     }
+    else {
+        alert("Please enter the month correctly");
+    }
+
 
 
 
 }
 
 
+
 function customCalculate() {
 
-    var inputIncome2 = documet.getElementById('customeincome').value;
-    var inputNecessities2 = documet.getElementById("customnecessities").value;
-    var inputSavings2 = documet.getElementById("customnsavings").value;
+    //Initializes user input
+    var inputIncome2 = document.getElementById("customincome").value;
+    var inputNecessities2 = document.getElementById("customnecessities").value;
+    var inputSavings2 = document.getElementById("customsavings").value;
+    var select = document.getElementById("chooseBudget").value
 
-    var spent = inputIncome2 - inputNecessities2;
 
-    alert(spent);
+    //Calculate the total amount spent
+    var totalSpent = inputIncome2 - inputNecessities2 - inputSavings2;
+
+
+    //Calculates what can be spent based on selection of monthly or yearly (user input)
+    //Make sure to change budget choosing user input as drag down
+    if (select == "monthly" || select == "Monthly") {
+        //Calculates monthly budget
+        var monthlyBudget = totalSpent / 12
+        var calculatedBudget2 = Math.floor(monthlyBudget* 100) / 100
+        alert(calculatedBudget2);
+    } else if (select == "yearly" || select == "Yearly") {
+        //Calculates yearly budget
+        var yearlyBudget = totalSpent
+        var calculatedBudget2 = Math.floor(yearlyBudget*100) / 100;
+        alert(calculatedBudget2);
+    } else {
+        alert("Please enter monthly or yearly")
+    }
+
+    
+
+
+
 
 }
